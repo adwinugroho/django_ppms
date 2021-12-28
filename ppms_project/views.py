1dd167fb50b9cd143a3316ef5d6e3bf967ffc406
adwin-nugroho <adwin.nugroho@diaconmed.com>
2021-12-28 17:00:03 +0000

    change 2

--- ppms_project/views.py
+++ ppms_project/views.py
@@ -83,43 +83,48 @@ def submitPatient(request):
     sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
     sock.connect((bd_addr, port))
     jam_file = str(datetime.now().strftime('%Y%m%d_%H%M'))
-    # testing with dummy data
+    print('Connected to ', bd_addr)
+
+    #define variabel
     suhu = '0'
     spo = '0'
     resp = '0'
     hr = '0'
+
     a = sock.recv(1024)
     jam = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     c = '' 
     for i in range(len(a)):
         c = c + ' ' + str(a[i])
-        #grab data
+            # f.writelines('\n')
+            # f.writelines(jam + ' ' +str(len(a)) +' ' + c)
+            
+            #grab data
         index = len(a)
         if index is 6:
             if a[5] in range (232,238):
-                resp = str(a[4])
+                    resp = str(a[4])
         elif index is 8:
             suhu = str(a[5]) + '.' + str(a[6])
         elif index is 16:
             spo = str(a[13])
             hr = str(a[14])
-        
         #cross check data
         if float(suhu) < 25 or float(suhu) > 40:
             suhu = '0'
-        
+            
         if float(spo) > 100:
             spo = '0'
-        
+            
         if float(hr) > 100:
             hr = '0'
-        
+            
         if float(resp) > 25:
             resp = '0'
         print(jam, suhu, spo, hr, resp)
-    # end for
-    # close connection sock
+            
     sock.close()
+    # f.close()
     # init and save to session
     request.session['jam'] = jam
     request.session['suhu'] = suhu
