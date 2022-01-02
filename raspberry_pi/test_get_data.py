016641f72417665885ddd0e56fd49b762d00ea49
adwin-nugroho <adwin.nugroho@diaconmed.com>
2022-01-02 10:41:11 +0000

    test new flow

--- raspberry_pi/test_get_data.py
+++ raspberry_pi/test_get_data.py
@@ -3,7 +3,7 @@ import time
 import datetime
 
 lines = []
-with open('/home/pi/Documents/PPMS/py3/django_ppms/raspberry_pi/file_login.txt', 'a') as fl:
+with open('/home/pi/Documents/PPMS/py3/django_ppms/raspberry_pi/file_login.txt', 'r') as fl:
     lines = fl.readlines()
 
 print('Check br address from login file: ', lines[0])    
